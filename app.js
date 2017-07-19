d0a884c9c30cdec3f3f3ec778872c28c18fc0814
Yusuke Ono <o.yusuke.od@gmail.com>
2017-07-20 01:39:55 +0900

    add log4js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var log4js = require('log4js');
+// var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -9,6 +10,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+log4js.configure('./log4js.config.json');
+var logger = log4js.getLogger('system');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +19,8 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(log4js.connectLogger(logger,{level:'auto'}));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
