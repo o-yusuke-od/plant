12001abcafdb4607355b87fca0957fd6e8d88e2d
YusukeOno <o.yusuke.od@gmail.com>
2017-08-09 19:00:27 +0900

    add canvas js

--- routes/detail/index.js
+++ routes/detail/index.js
@@ -3,19 +3,23 @@ const express = require('express');
 const router = express.Router();
 const TPlant = require('../../database/api/TPlant.js');
 const time = require('moment');
-const start = time().subtract(1,'day').format("YYYY-MM-DD")+" 00:00:00";
-const end = time().format("YYYY-MM-DD")+ " 00:00:00";
+const start = time().format("YYYY-MM-DD")+ " 00:00:00";
+const end = time().add(1,'day').format("YYYY-MM-DD")+" 00:00:00";
 
 router.get('/:id',function(req,res,next) {
     Promise.all([
         TPlant.findById(req.params.id),
-        TPlant.findNameById(req.params.id)
+        TPlant.findNameById(req.params.id),
+        TPlant.findListByDate(req.params.id,start,end)
     ]).then(function(data) {
         return res.render('detail',{
-           data1:data[0],
-           data2:data[1]
+            data1:data[0],
+            data2:data[1],
+            new_data:data[data.length-1]
         });
     });
+
+    return res.render('detail');
 });
 
 module.exports = router;
