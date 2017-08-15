a65cc884d2dab513536ea769bf50beabd33bdc5f
Yusuke Ono <o.yusuke.od@gmail.com>
2017-08-15 13:06:07 +0900

    merge genre

--- routes/top/index.js
+++ routes/top/index.js
@@ -3,23 +3,23 @@
 const express = require('express');
 const router = express.Router();
 const TPlant = require('../../database/api/TPlant.js');
-const top_data =[{}];
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // TPlant.findId().then(function(data){
-    //     Object.keys(data).forEach(function(val) {
-    //         top_data[val] = {"id":data[val]["id"],"name":data[val]["name"]};
-    //     });
-    //     return res.render('index',{
-    //         data:top_data
-    //     });
-    // });
-    let top_data = [ { id: 1, name: 'aico' } ];
-    return res.render('index',{
-        data:top_data
-    });
+     let top_data =[{}];
+     TPlant.findId().then(function(data){
+         Object.keys(data).forEach(function(val) {
+             top_data[val] = {
+                 "id":data[val]["id"],
+                 "name":data[val]["name"],
+                 "genre":data[val]["genre"] 
+             };
+         });
+         return res.render('index',{
+             data:top_data
+         });
+     });
 });
 
 module.exports = router;
