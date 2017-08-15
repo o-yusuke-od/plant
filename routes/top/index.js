3ab76c0e355d08d3134ea7536f6d7bd1d86cf883
Yusuke Ono <o.yusuke.od@gmail.com>
2017-08-15 11:15:48 +0900

    fix test2branch

--- routes/top/index.js
+++ routes/top/index.js
@@ -3,14 +3,17 @@
 const express = require('express');
 const router = express.Router();
 const TPlant = require('../../database/api/TPlant.js');
+const top_data =[{}];
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     TPlant.findId().then(function(data){
-        console.log(data);
+        Object.keys(data).forEach(function(val) {
+            top_data[val] = {"id":data[val]["id"],"name":data[val]["name"]};
+        });
         return res.render('index',{
-            data:data
+            data:top_data
         });
     });
 });
