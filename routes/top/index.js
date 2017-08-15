cbd4c07e64c0a10b2b755bdd15b7a0b4699c2cdc
YusukeOno <o.yusuke.od@gmail.com>
2017-08-15 12:57:40 +0900

    add views template

--- routes/top/index.js
+++ routes/top/index.js
@@ -8,13 +8,17 @@ const top_data =[{}];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    TPlant.findId().then(function(data){
-        Object.keys(data).forEach(function(val) {
-            top_data[val] = {"id":data[val]["id"],"name":data[val]["name"]};
-        });
-        return res.render('index',{
-            data:top_data
-        });
+    // TPlant.findId().then(function(data){
+    //     Object.keys(data).forEach(function(val) {
+    //         top_data[val] = {"id":data[val]["id"],"name":data[val]["name"]};
+    //     });
+    //     return res.render('index',{
+    //         data:top_data
+    //     });
+    // });
+    let top_data = [ { id: 1, name: 'aico' } ];
+    return res.render('index',{
+        data:top_data
     });
 });
 
