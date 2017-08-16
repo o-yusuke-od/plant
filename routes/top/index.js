660cf66e66eb591ef64bba609307ea9b2be83944
YusukeOno <o.yusuke.od@gmail.com>
2017-08-16 11:50:01 +0900

    rename api/TPlant

--- routes/top/index.js
+++ routes/top/index.js
@@ -2,27 +2,23 @@
 
 const express = require('express');
 const router = express.Router();
-const TPlant = require('../../database/api/TPlant.js');
+const api = require('../../database/api');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-     // let top_data =[{}];
-     // TPlant.findId().then(function(data){
-     //     Object.keys(data).forEach(function(val) {
-     //         top_data[val] = {
-     //             "id":data[val]["id"],
-     //             "name":data[val]["name"],
-     //             "genre":data[val]["genre"] 
-     //         };
-     //     });
-     //     return res.render('index',{
-     //         data:top_data
-     //     });
-     // });
-    let top_data = [{"id":1,"name":"aico","genre":"mini tomato"}];
-    return res.render('index',{
-        data:top_data
+    let top_data =[{}];
+    api.findId().then(function(data){
+        Object.keys(data).forEach(function(val) {
+            top_data[val] = {
+                "id":data[val]["id"],
+                "name":data[val]["name"],
+                "genre":data[val]["genre"] 
+            };
+        });
+        return res.render('index',{
+            data:top_data
+        });
     });
 });
 
